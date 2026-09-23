25c784b6fa6c143c111ffc870fb0cabb07700440
melisaaltinyelek <melisaaltinyelek@gmail.com>
2026-09-23 18:10:27 +0200

    Filtered stock data and implemented user budget input

--- get_stock_data.py
+++ get_stock_data.py
@@ -1,9 +1,14 @@
+# %%
+
 import requests
 from dotenv import load_dotenv
 import os
 import json
 import pandas as pd
 from IPython.display import display
+import time
+
+# %%
 
 load_dotenv()
 
@@ -12,25 +17,98 @@ api_key = os.getenv("API_KEY")
 # Stock symnbols to search for in XETRA
 stock_list = ["AMD.DEX", "AIR.DEX", "SIE.DEX"]
 
-df_values = []
 
-for stock in stock_list:
-    url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={stock}&apikey={api_key}"
-    response = requests.get(url)
-    data = response.json()
+def get_stock_data(stock_list):
 
-    if "Global Quote" in data:
-        quote = data["Global Quote"]
+    df_values = []
 
-        # for key, value in quote.items():
-        #     print(f"{key}: {value}")
+    for stock in stock_list:
+        url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={stock}&apikey={api_key}"
+        response = requests.get(url)
+        data = response.json()
+        time.sleep(3)
 
-        df_values.append(quote)
+        if "Global Quote" in data:
+            quote = data["Global Quote"]
 
-    else:
-        print(json.dumps(data, indent=4))
+            # for key, value in quote.items():
+            #     print(f"{key}: {value}")
 
-print(df_values)
+            df_values.append(quote)
 
-df = pd.DataFrame.from_dict(df_values)
+        else:
+            print(json.dumps(data, indent=4))
+
+    print(df_values)
+    df = pd.DataFrame.from_dict(df_values)
+
+    return df
+
+
+df = get_stock_data(stock_list)
+# ndisplay(df)
+
+# %%
+
+
+def clean_df_cols(df):
+
+    new_df_cols = []
+
+    for col in df.columns:
+        print(col)
+        new_col = col.split(" ", 1)[1]
+        new_col = new_col.title()
+        new_df_cols.append(new_col)
+
+    # print(new_df_cols)
+
+    df.columns = new_df_cols
+
+    # print(df.columns)
+
+    return df
+
+
+df = clean_df_cols(df)
 display(df)
+
+# %%
+
+df.to_csv("data/stock_data.csv", index=False)
+
+# %%
+
+
+def get_user_budget():
+
+    budget = input("Enter your budget (EUR): ")
+
+    try:
+        budget = float(budget)
+        return budget
+
+    except ValueError:
+        print("Invalid input. Please enter a numeric value.")
+        return get_user_budget()
+
+
+user_budget = get_user_budget()
+
+
+# %%
+
+stock_df = pd.read_csv("data/stock_data.csv")
+stock_df.dtypes
+
+
+def display_stocks(budget):
+
+    filtered_df = stock_df[stock_df["Price"] <= budget]
+    if not filtered_df.empty:
+        return filtered_df
+    else:
+        print(f"No stock has been found in the budget of {budget}€.")
+
+
+display_stocks(budget=user_budget)
